531667c4821e3de9c160789e149e98caae38892c
uweradiane <uweradiane27@gmail.com>
2023-10-07 14:50:36 -0400

    index.php formulaire

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <form method="post" action="result.php">
+            Mot De Passe:
+            <input type="text" name="password">
+           
+            <input type="submit">
+        </form>
+    </body>
+</html>
