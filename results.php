4746dd655f3f7ff1cd7c822c1cf0afe0441a9e94
uweradiane <uweradiane27@gmail.com>
2023-10-08 15:56:17 -0400

    functions

---
+++ results.php
@@ -0,0 +1,27 @@
+<?php
+require_once("functions.php");
+var_dump($_POST);
+if($_POST){
+    $name = $_POST['mot de passe'];
+    if (empty($password)){
+        echo "</br> password is empty ";
+    }
+    else{
+        echo "<br> Mon nom est:  " . $password;
+    }
+    $nameLengthIsValid = nameLengthIsvalid($_POST['mot de passe']);
+    echo '</br>';
+    var_dump($passwordLengthIsValid);
+    if(!$passwordLengthIsValid['isValid']){
+        //on fait note traitement
+    }
+};
+    echo '</br>';
+    $saltedpassword = addSalt($_POST['mot de password']);
+    var_dump($saltedpassword);
+    $encodepassword = encodeName($saltedpassword);
+     echo '</br>';
+    var_dump ($encodepassword);
+
+
+?>
